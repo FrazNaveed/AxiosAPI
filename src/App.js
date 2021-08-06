e1d79bca6aa91d93c45881a4e240eabeb90fb718
fraz <faraznaveed8.fn@gmail.com>
2021-08-06 16:21:01 +0500

    first commit for axios

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from 'react';
+import axios from 'axios';
+
 
 function App() {
+
+
+  const  [num, setNum] = useState();
+  const  [name, setName] = useState();
+  const  [moves, setMoves] = useState();
+
+
+  useEffect(()=>{
+  async function getData(){
+    const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${num}`)
+    console.log(res.data.name);
+     setName(res.data.name);
+     setMoves(res.data.moves.length);
+  }
+  getData();
+  });
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1> You chose: <span style = {{color:'red'}}>   {num}  </span></h1>
+    <h1> Name is: <span style = {{color:'red'}}>     {name}  </span></h1>
+    <h1> Moves are <span style = {{color:'red'}}>    {moves} </span></h1>
+    <select value = {num} onChange={(event)=>{setNum(event.target.value)}}>
+
+
+   <option value = '1'> 1 </option>
+   <option value = '2'> 2 </option>
+   <option value = '3'> 3 </option>
+   <option value = '4'> 4 </option>
+   <option value = '5'> 5 </option>
+   <option value = '6'> 6 </option>
+   <option value = '7'> 7 </option>
+   
+ </select>
+    </>
   );
 }
 
